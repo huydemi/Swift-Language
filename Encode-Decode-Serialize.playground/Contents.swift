c04d3f47c888092a5e00efd846052835cafd3516
Dang Quoc Huy <huysamdua@yahoo.com>
2018-08-28 12:30:19 +0700

    JSONEncoder and JSONDecoder

--- Encode-Decode-Serialize.playground/Contents.swift
+++ Encode-Decode-Serialize.playground/Contents.swift
@@ -14,3 +14,19 @@ struct Employee: Codable {
 struct Toy: Codable {
   var name: String
 }
+
+let toy1 = Toy(name: "Teddy Bear");
+let employee1 = Employee(name: "John Appleseed", id: 7, favoriteToy: toy1)
+
+let jsonEncoder = JSONEncoder()
+let jsonData = try jsonEncoder.encode(employee1)
+
+print(jsonData)
+
+let jsonString = String(data: jsonData, encoding: .utf8)
+print(jsonString)
+// {"name":"John Appleseed","id":7,"favoriteToy":{"name":"Teddy Bear"}}
+
+let jsonDecoder = JSONDecoder()
+let employee2 = try jsonDecoder.decode(Employee.self, from: jsonData)
+
