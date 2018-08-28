eb84b9791c9c65ad991bfa27975ebd5c94e9166e
Dang Quoc Huy <huysamdua@yahoo.com>
2018-08-28 12:53:40 +0700

    add OOP, properties

---
+++ OOP.playground/Contents.swift
@@ -0,0 +1,17 @@
+/*
+ Object Oriented Programming in Swift
+ https://www.raywenderlich.com/599-object-oriented-programming-in-swift
+ */
+
+import UIKit
+
+// 1
+class Instrument {
+  // 2
+  let brand: String
+  // 3
+  init(brand: String) {
+    //4
+    self.brand = brand
+  }
+}
